a56e49a6a06c5cb71871cea170f8ff4c7bfb83b9
Nasiba <nasyrova.nasiba@gmail.com>
2021-03-15 22:11:59 +0500

    finished 1st assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HomeworkLesson16
 {
@@ -6,7 +7,24 @@ namespace HomeworkLesson16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FirstAssignment();
+        }
+
+        static void FirstAssignment()
+        {
+            Console.Write("Enter number for the 1st assignment: ");
+            var s = Console.ReadLine();
+            var result = s.ToCharArray().Reverse().ToArray();
+            Console.WriteLine("Reversed number: ");
+            foreach (var VARIABLE in result)
+            {
+                Console.Write(VARIABLE);
+            }
+        }
+
+        static void SecondAssignment()
+        {
+            
         }
     }
 }
